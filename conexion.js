707e12a7de4752a0528e6a18269f6659e9c0ccb5
Ever Alonso <everar.ti22@utsjr.edu.mx>
2023-08-10 09:39:19 -0600

    Actualizacion

--- conexion.js
+++ conexion.js
@@ -3,11 +3,11 @@ var AlumnoModelo = require("./modelos/alumno");
 var ReservaModelo = require("./modelos/reserva");
 require("dotenv").config();
 
-var db=process.env.DB_MYSQL_LOCAL;
-var usuario=process.env.USUARIO_MYSQL_LOCAL;
-var password=process.env.PASSWORD_MYSQL_LOCAL;
-var host=process.env.HOST_MYSQL_LOCAL;
-var port=process.env.PORT_MYSQL_LOCAL;
+var db=process.env.DB_MYSQL_REMOTO;
+var usuario=process.env.USUARIO_MYSQL_REMOTO;
+var password=process.env.PASSWORD_MYSQL_REMOTO;
+var host=process.env.HOST_MYSQL_REMOTO;
+var port=process.env.PORT_MYSQL_REMOTO;
 
 var conexion = new Sequelize(db,usuario,password,{
     host:host,
@@ -15,7 +15,7 @@ var conexion = new Sequelize(db,usuario,password,{
     dialect:"mysql",
     dialectOptions:{
         ssl:{
-            rejectUnauthorized:false
+            rejectUnauthorized:true
         },
     }
 });
